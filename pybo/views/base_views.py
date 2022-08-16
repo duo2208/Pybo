e72eb8cb1bac764b90658cfa84d13e2d40862566
jimin <duo2208@likelion.org>
2022-08-16 16:13:28 +0900

    검색 오류 해결

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,16 +1,26 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
 
-from ..models import Answer, Question
-
+from ..models import Question
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
-    page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')      # 검색어
+    page = request.GET.get('page', '1') # 페이지
+    
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  
+            Q(content__icontains=kw) |  
+            Q(author__username__icontains=kw) |  
+            Q(answer__author__username__icontains=kw)  
+        ).distinct()
+
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
